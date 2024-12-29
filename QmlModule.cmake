357c3350615d3e88fa15a7e9c079dfdaf9502521
kraxarn <kraxie@protonmail.com>
2024-12-29 19:23:41 +0100

    Initial project files

---
+++ QmlModule.cmake
@@ -0,0 +1,16 @@
+# QML files are in qml/
+#qt_policy(SET QTP0004 NEW)
+
+qt_add_qml_module(${PROJECT_NAME}
+	URI ${PROJECT_NAME}_app
+	VERSION ${PROJECT_VERSION}
+	NO_RESOURCE_TARGET_PATH
+	QML_FILES
+	qml/Main.qml
+	qml/Icon.qml
+	RESOURCES
+	res/font/regular.ttf
+	res/font/bold.ttf
+	res/font/light.ttf
+	res/icon/speaker.svg
+)
---
+++ readme.md
@@ -0,0 +1,2 @@
+# hitster-scanner
+open source, and improved, scanner for the hitster board game
