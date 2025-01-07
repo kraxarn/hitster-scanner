a8895810de184a69a1ecc740ea019a46616dd8ff
kraxarn <kraxie@protonmail.com>
2025-01-07 12:14:24 +0100

    Add swedish translation

--- ts/hitster_sv.ts
+++ ts/hitster_sv.ts
@@ -2,11 +2,89 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="sv" sourcelanguage="en">
 <context>
+    <name>AuthPage</name>
+    <message>
+        <location filename="../qml/AuthPage.qml" line="26"/>
+        <source>Please login to continue</source>
+        <translation>Logga in för att fortsätta</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceButton</name>
+    <message>
+        <location filename="../qml/DeviceButton.qml" line="11"/>
+        <source>(no device)</source>
+        <translation>(ingen enhet)</translation>
+    </message>
+    <message>
+        <location filename="../qml/DeviceButton.qml" line="31"/>
+        <source>Select device</source>
+        <translation>Välj enhet</translation>
+    </message>
+</context>
+<context>
+    <name>HomePage</name>
+    <message>
+        <location filename="../qml/HomePage.qml" line="32"/>
+        <source>Start new game</source>
+        <translation>Nytt spel</translation>
+    </message>
+    <message>
+        <location filename="../qml/HomePage.qml" line="44"/>
+        <source>How to play</source>
+        <translation>Regler</translation>
+    </message>
+    <message>
+        <location filename="../qml/HomePage.qml" line="50"/>
+        <source>FAQ</source>
+        <translation>FAQ</translation>
+    </message>
+    <message>
+        <location filename="../qml/HomePage.qml" line="56"/>
+        <source>Change account</source>
+        <translation>Byt konto</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <location filename="../qml/LoginPage.qml" line="26"/>
+        <source>Login with Spotify</source>
+        <translation>Logga in med Spotify</translation>
+    </message>
+</context>
+<context>
+    <name>Logo</name>
+    <message>
+        <location filename="../qml/Logo.qml" line="16"/>
+        <source>Hitster</source>
+        <translation>Hitster</translation>
+    </message>
+</context>
+<context>
     <name>Main</name>
     <message>
-        <location filename="../qml/Main.qml" line="8"/>
-        <source>Open Hitster</source>
-        <translation type="unfinished"></translation>
+        <location filename="../qml/Main.qml" line="9"/>
+        <source>Hitster</source>
+        <translation>Hitster</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="58"/>
+        <source>How to play</source>
+        <translation>Regler</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="66"/>
+        <source>FAQ</source>
+        <translation>FAQ</translation>
+    </message>
+</context>
+<context>
+    <name>ScanPage</name>
+    <message>
+        <location filename="../qml/ScanPage.qml" line="23"/>
+        <source>Scan QR code</source>
+        <translation>Skanna QR kod</translation>
     </message>
 </context>
 </TS>
